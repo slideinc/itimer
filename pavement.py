63bbc57cf351f2b649f451bdd970d2f0cb35d0f6
Travis J Parker <travis.parker@gmail.com>
2010-06-09 16:29:22 -0700

    gitignore, license, distribution boilerplate

---
+++ pavement.py
@@ -0,0 +1,61 @@
+import errno
+import os
+from setuptools import Extension
+
+from paver.easy import *
+from paver.path import path
+from paver.setuputils import setup
+
+
+setup(
+    name="itimer",
+    description="alarm signal delivery with setitimer(2) and getitimer(2)",
+    version="1.0",
+    py_modules=['itimer'],
+    ext_modules=[Extension(
+        '_itimer',
+        ['itimermodule.c'],
+        include_dirs=('.',),
+        extra_compile_args=['-Wall'])],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        #"License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Programming Language :: C",
+        "Topic :: System",
+    ]
+)
+
+MANIFEST = (
+    "setup.py",
+    "paver-minilib.zip",
+    "itimermodule.c",
+)
+
+@task
+def manifest():
+    path('MANIFEST.in').write_lines('include %s' % x for x in MANIFEST)
+
+@task
+@needs('generate_setup', 'minilib', 'manifest', 'setuptools.command.sdist')
+def sdist():
+    pass
+
+@task
+def clean():
+    for p in map(path, ('itimer.egg-info', 'dist', 'build', 'MANIFEST.in')):
+        if p.exists():
+            if p.isdir():
+                p.rmtree()
+            else:
+                p.remove()
+    for p in path(__file__).abspath().parent.walkfiles():
+        if p.endswith(".pyc") or p.endswith(".pyo"):
+            try:
+                p.remove()
+            except OSError, exc:
+                if exc.args[0] == errno.EACCES:
+                    continue
+                raise
