9075dc68a4cfb156daab7492a091694fa027da2e
Travis J Parker <travis.parker@gmail.com>
2010-06-15 10:51:45 -0700

    bump version for a package fix and release

--- pavement.py
+++ pavement.py
@@ -10,7 +10,7 @@ from paver.setuputils import setup
 setup(
     name="itimer",
     description="alarm signal delivery with setitimer(2) and getitimer(2)",
-    version="1.0",
+    version="1.0.1",
     license="bsd",
     author="Libor Michalek",
     author_email="libor@pobox.com",
